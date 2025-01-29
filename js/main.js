3a1ccd96e0fab2061b22f88740e2c5a1b70f02bb
alessandro-sartini <ale.sartini.98@gmail.com>
2025-01-29 12:58:51 +0100

    add: hover animation and hidden

--- js/main.js
+++ js/main.js
@@ -11,16 +11,16 @@ axios.get(apiKey)
 
             cardContainer.innerHTML +=
             
-          `  <div class="col-sm-12 col-md-6 col-lg-4">
+          `  <div class="col-sm-12 col-md-6 col-lg-4 ">
 
-                <div class="card m-3" style="width: 100%;">
+                <div class="card m-3 rotate-zoom" style="width: 100%;">
 
-                    <figure class="position-relative">
-                        <img class="position-absolute top-0 start-50 translate-middle" src="./img/pin.svg" alt="pin">
+                    <figure class="position-relative rotation-none">
+                        <img class="position-absolute top-0 start-50 translate-middle " src="./img/pin.svg" alt="pin" style="width:2.5rem;">
                     </figure>
 
                     <a href="">
-                        <img src= "${element.url}" class="card-img-top p-3" alt="...">
+                        <img src= "${element.url}" class="card-img-top p-3 img-fluid" alt="...">
                     </a>
                         
                     <div class="card-body">
