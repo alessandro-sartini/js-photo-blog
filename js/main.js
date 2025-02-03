fe8f57a23198122fffe5e77fd702e7ed77f5798e
alessandro-sartini <ale.sartini.98@gmail.com>
2025-02-03 11:46:15 +0100

    finish

--- js/main.js
+++ js/main.js
@@ -10,27 +10,6 @@ axios
       const cardContainer = document.querySelector(".container .row");
 
         
-        //! 2th version no animate
-        // cardContainer.innerHTML +=
-        //     `
-        //      <div class="col-12 col-sm-6 col-md-6 col-lg-4 col-xl-4">
-        //             <div class="card mb-4">
-        //                 <div class="position-relative">
-        //                     <div class="position-absolute top-0 start-50 translate-middle">
-        //                         <img src="./img/pin.svg" alt="pin">
-        //                     </div>
-        //                 </div>
-        //                 <img width="100%" src="${element.url}" class="card-img-top p-3" alt="${element.title}">
-        //                 <div class="card-body">
-        //                 <p class="card-text">${element.date}</p> 
-        //                 <h5 class="card-title fw-bold">${element.title}</h5>
-        //                 </div>
-        //             </div>
-        //         </div>
-        //     </div>
-            
-        //     `
-        
         //! first version animated
         // si Usa innerHTML per inserire tutta la struttura img
 
@@ -44,7 +23,7 @@ axios
             </figure>
 
             <a href="#" class="rounded" id="${element.id}"  >
-              <img  src= "${element.url}" class="card-img-top p-3 img-fluid" alt=${element.title}>
+              <img  src= "${element.url}" class="card-img-top p-3 img-fluid" alt=${element.title} data-id="${element.id}">
             </a>
                         
             <div class="card-body">
@@ -58,8 +37,7 @@ axios
       `;
 
       console.log(element.id)
-
-
+        
 
         // Aggiungo un listener per il click sulle card
         document.querySelector(".container .row").addEventListener("click", function (event) {
@@ -106,12 +84,3 @@ axios
     document.querySelector(".opacity-container").classList.add("d-none"); // Nascondi il pop-up
   });
 
-// const pressCard = document.getElementById('pressCard');
-// const popUp = document.getElementById('popUp');
-
-// pressCard.addEventListener('click', function (event) {
-//     event.preventDefault();
-//     popUp.classList.remove('d-none');
-
-
-// })
